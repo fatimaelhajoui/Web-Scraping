d2d7e45e70839ab8743ab3cb41cb46eeaae40bf0
fatimaelhajoui <113965747+fatimaelhajoui@users.noreply.github.com>
2024-07-29 15:49:10 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -4,7 +4,7 @@ import csv
 from itertools import zip_longest
 
 page_num=0
-url= 'https://www.avito.ma/fr/fes/ordinateurs_portables/dell--%C3%A0_vendre?o={page_num}'
+url= 'https://www.avito.ma/fr/fes/ordinateurs_portables/dell--%C3%A0_vendre?o={page_num}' # enter the name of the website here I use avito as an exemple
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
            'Referer' : 'https://www.avito.ma/'
            }
